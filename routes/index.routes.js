e2cf9c052bfbff5faf8cbab4b4e7056f32ec1777
paulocaetano96 <paulocaetano96@gmail.com>
2023-02-19 22:45:59 +0000

    index, style.css, routes upadte

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,4 +6,17 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+/* login route */
+router.get("/login", (req, res, next) => {
+  res.render("auth/login.hbs");
+});
+
+/* sign up route */
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup.hbs");
+});
+
+
+
+
 module.exports = router;
