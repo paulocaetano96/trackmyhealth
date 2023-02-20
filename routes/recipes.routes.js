dd4910803c3e2e16fc5234c082f38bee554d7d4f
paulocaetano96 <paulocaetano96@gmail.com>
2023-02-20 13:37:31 +0000

    route update

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,25 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
+const axios = require("axios");
 
-
-
-router.get('/test', async (req,res,next) => {
+router.get("/test", async (req, res, next) => {
   try {
-    let response = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&app_id=aec1e99b&app_key=09ad4047d6477e600633dadb368f19eb&ingr=4-10&diet=balanced&cuisineType=Mediterranean&mealType=Lunch&dishType=Main%20course&time=5-30&imageSize=REGULAR&random=true`)
+    let response = await axios.get(
+      `https://api.edamam.com/api/recipes/v2?type=public&app_id=aec1e99b&app_key=09ad4047d6477e600633dadb368f19eb&ingr=4-10&diet=balanced&cuisineType=Mediterranean&mealType=Lunch&dishType=Main%20course&time=5-30&imageSize=REGULAR&random=true`
+    );
 
     let data = response.data.hits;
-    console.log(data);
-    res.render('recipes/my-recipes.hbs',{data:data});
-    
-  }
-  catch (err) {
+    console.log(data.ingredients[0]);
+    res.render("recipes/my-recipes.hbs", { data: data });
+  } catch (err) {
     console.log(err);
     next(err);
-    
-    
-  }})
-
-
+  }
+});
 
 module.exports = router;
